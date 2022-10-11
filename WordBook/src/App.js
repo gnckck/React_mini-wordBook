39c2c5208eaebad0baa757f2fc5efffd664b13f8
gnckck <jjnwlsk@naver.com>
2022-10-12 02:35:55 +0900

    POST(생성), useNavigate()
    POST 메소드로 단어를 생성하고 useNavigate() 를 통해 보내고싶은 주소를 넣어주면 해당페이지로 이동 ( Link to 처럼 a 태그를 사용하지않고 페이지 전환시킬때 유용하게 사용)

--- WordBook/src/App.js
+++ WordBook/src/App.js
@@ -4,7 +4,9 @@ import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from './component/Header';
 import EmptyPage from './component/EmptyPage';
+import CreateWord from './component/CreateWord';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateDay from './component/CreateDay';
 
 function App() {
  return (
@@ -20,6 +22,8 @@ function App() {
       {/* 콜론을 붙여주면 day 1로 들어왔을 때 day 라는 변수로 1의 값을 얻을수 있음 */}
       
       <Route path="*" element={<EmptyPage/>} />
+      <Route path="/create_word" element={<CreateWord/>} />
+      <Route path="/create_day" element={<CreateDay/>} />
       </Routes>
       </div>
       </BrowserRouter>
